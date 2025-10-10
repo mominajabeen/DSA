e9964bef9f7852fc049b6f9690eed6de88bb0ece
Momina Jabeen <momina.jabeen8@gmail.com>
2025-10-10 22:57:34 +0500

    Time: 126 ms (89.44%), Space: 151.6 MB (61.62%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -3,11 +3,11 @@ public:
     int maximumEnergy(vector<int>& energy, int k) {
         int n = energy.size();
         vector<int> dp(n);
-        int result = INT_MIN;
-        for (int i = n - 1; i >= 0; --i) {
+        int res = INT_MIN;
+        for(int i = n - 1; i >= 0; i--){
             dp[i] = energy[i] + ((i + k < n) ? dp[i + k] : 0);
-            result = max(result, dp[i]);
+            res = max(res, dp[i]);
         }
-        return result;
+        return res;
     }
 };
